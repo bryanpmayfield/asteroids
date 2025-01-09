9c8ae580ad6b446df94f1289826ad62240ea7912
bryanpmayfield <94194139+bryanpmayfield@users.noreply.github.com>
2025-01-08 23:23:53 -0500

    Added player to groups, finished L8

--- main.py
+++ main.py
@@ -7,16 +7,31 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    # Creating groups 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     dt = 0
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+
 
     # Creating the game loop 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for item in updatable:
+            item.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        
+        for item in drawable:
+            item.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60) / 1000
     print("Starting asteroids!")
